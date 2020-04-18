ad3c9c9fad60e9e05b666afc56c66616ef5102f9
Connor Bach <cbach120@gmail.com>
2020-04-17 23:00:16 -0400

    adding multiple dinosaurs

--- src/globalGameFunctions.c
+++ src/globalGameFunctions.c
@@ -7,7 +7,10 @@
 int n_dinos = 5;
 
 Texture2D playerTexture;
-Texture2D raptorTexture;
+Texture2D greenRaptorTexture;
+Texture2D blueRaptorTexture;
+Texture2D orangeBrontTexture;
+Texture2D purpleRexTexture;
 
 /* Init 
  * Called to setup game
@@ -15,7 +18,10 @@ Texture2D raptorTexture;
 GameState *InitGameState() {
 	//load textures 
 	playerTexture = LoadTexture("../assets/PlayerCharacter.png");
-	raptorTexture = LoadTexture("../assets/dino1.png");
+	greenRaptorTexture = LoadTexture("../assets/dino1.png");
+	blueRaptorTexture = LoadTexture("../assets/dino2.png");
+	orangeBrontTexture= LoadTexture("../assets/dino3.png");
+	purpleRexTexture = LoadTexture("../assets/dino4.png");
 
 	// setup game state
 	GameState *state = malloc(sizeof(GameState));
@@ -25,7 +31,7 @@ GameState *InitGameState() {
 
 	Dino **dinos = malloc(sizeof(Dino) * n_dinos);
 	for(int i = 0; i < n_dinos; i++) {
-		dinos[i] = dinoCreate(GetRandomValue(0, 800), GetRandomValue(0, 450), GreenRaptor);
+		dinos[i] = dinoCreate(GetRandomValue(0, 800), GetRandomValue(0, 450), GetRandomValue(0, 3));
 	}
 	state->dinos = dinos;
 
@@ -53,8 +59,28 @@ void Draw(GameState *state) {
 	ClearBackground(RAYWHITE);
 
 	Player *player = state->player;
-
 	DrawTexture(playerTexture, player->x, player->y, RAYWHITE);
+
+    Dino **dinos = state->dinos;
+    for(int i = 0; i < n_dinos; i++) {
+        Texture2D dinoTexture;
+        switch(dinos[i]->type) {
+            case GreenRaptor:
+                dinoTexture = greenRaptorTexture;
+                break;
+            case BlueRaptor:
+                dinoTexture = blueRaptorTexture;
+                break;
+            case OrangeBront:
+                dinoTexture = orangeBrontTexture;
+                break;
+            case PurpleRex:
+                dinoTexture = purpleRexTexture;
+                break;
+        }
+
+        DrawTexture(dinoTexture, dinos[i]->x, dinos[i]->y, RAYWHITE);
+    }
 
 	EndDrawing();
 }
